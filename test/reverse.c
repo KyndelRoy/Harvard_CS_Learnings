defba280d7106091b261e46de904504fa621cabc
KyndelRoy <k.bangoy.suarez@gmail.com>
2025-08-19 23:25:51 +0800

    test feature to showcase dsa mastery

---
+++ test/reverse.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void reverse(int numbers[], int size);
+int main(void){
+    int numbers[] = {5,4,3,2,1};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    reverse(numbers, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+}
+
+void reverse(int numbers[], int size) {
+    int start = 0;
+    int end = size - 1;
+    int temp;
+    while (start < end) {
+        // Swap the elements at start and end
+        int temp = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = temp;
+
+        // Move towards the middle
+        start++;
+        end--;
+    }
+}
